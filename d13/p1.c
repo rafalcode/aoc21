b305b622d0185863ae1ef9a2204eca914119ca5f
rafalco <ramonfallon@gmail.com>
2021-12-14 12:21:49 +0000

    sqaure obtained in p1.c

--- d13/p1.c
+++ d13/p1.c
@@ -38,6 +38,22 @@ typedef struct /* av_c, a vector fo simple integers*/
     int vbf, vsz, *m, *n;
 } av_c;
 
+void memexact_avc(av_c *avc)
+{
+    /* somewhat trivial, but idea is that, as avc is a container, it can be re-alloced inside a function */
+    avc->m=realloc(avc->m, avc->vsz*sizeof(int));
+    avc->n=realloc(avc->n, avc->vsz*sizeof(int));
+    return;
+}
+
+void free_avc(av_c *avc)
+{
+    free(avc->m);
+    free(avc->n);
+    free(avc);
+    return;
+}
+
 void optis(av_c *avc) // order only on m members
 {
     int i, j, t, t2;
@@ -56,12 +72,59 @@ void optis(av_c *avc) // order only on m members
     }
 }
 
+void uniq_avc(av_c *avc)
+{
+    int i;
+    // uniqueize
+    int m=avc->m[0];
+    int n=avc->n[0];
+    int k=1;
+    for(i=1;i<avc->vsz;++i) {
+        if((m==avc->m[i]) && (n==avc->n[i]))
+            continue;
+        else {
+            avc->m[k]=avc->m[i];
+            avc->n[k]=avc->n[i];
+            m=avc->m[i];
+            n=avc->n[i];
+            k++;
+        }
+    }
+    avc->vsz=k;
+    memexact_avc(avc);
+    return;
+}
+
+void proc_avc(av_c *avc, int xy, int v, int r, int c)
+{
+    int i;
+    if(xy==1) {
+        for(i=0;i<avc->vsz;++i) {
+            if(avc->n[i]>v) {
+//                 printf("%i->", avc->n[i]);
+                avc->n[i]=r-1-avc->n[i];
+//                printf("%i\n", avc->n[i]);
+            }
+        }
+    } else {
+        for(i=0;i<avc->vsz;++i) {
+            if(avc->m[i]>v) {
+                printf("%i->", avc->m[i]);
+                avc->m[i]=c-1-avc->m[i];
+                printf("%i\n", avc->m[i]);
+            }
+        }
+    }
+    optis(avc);
+    return;
+}
+
 void prt_avc(av_c *avc)
 {
     int i;
-    printf("Avec printed out:\n"); 
+    printf("Avec printed out rows before columns:\n"); 
     for(i=0;i<avc->vsz;++i)
-        printf("(%i,%i) ", avc->m[i], avc->n[i]);
+        printf("(%i,%i) ", avc->n[i], avc->m[i]);
     printf("\n"); 
     return;
 }
@@ -80,7 +143,7 @@ void findmx(av_c *avc)
     printf("grid %i rows x %i cols\n", rmx, cmx); 
     for(r=0;r<rmx;++r) {
         for(c=0;c<cmx;++c) {
- //           printf("%i,%i-%i,%i ", r, c, avc->n[k], avc->m[k]);
+            //           printf("%i,%i-%i,%i ", r, c, avc->n[k], avc->m[k]);
             if((c==avc->m[k]) && (r==avc->n[k])) {
                 putchar('#');
                 k++;
@@ -88,6 +151,63 @@ void findmx(av_c *avc)
                 putchar('.');
         }
         putchar('\n');
+    }
+
+    return;
+}
+
+void findmx0(av_c *avc, int rmx, int cmx)
+{
+    int i, r, c, k=0;
+    printf("grid %i rows x %i cols\n", rmx, cmx); 
+    int m=-1;
+    int n=-1;
+    int sum=0;
+    for(r=0;r<rmx;++r) {
+        for(c=0;c<cmx;++c) {
+//            printf("%i,%i-%i,%i ", r, c, avc->n[k], avc->m[k]);
+            if((c==avc->m[k]) && (r==avc->n[k])) {
+                putchar('#');
+                sum++;
+                m=avc->m[k];
+                n=avc->n[k];
+                k++;
+                while((m==avc->m[k]) && (n==avc->n[k]))
+                    k++;
+            } else {
+                putchar('.');
+            }
+        }
+        putchar('\n');
+    }
+    printf("sumdots=%i\n", sum);
+
+    return;
+}
+
+void findmx2(av_c *avc, int xy, int v)
+{
+    int i, r, c, k=0, rmx=0, cmx=0;
+    for(i=0;i<avc->vsz;++i) {
+        if(avc->m[i]>cmx)
+            cmx=avc->m[i];
+        if(avc->n[i]>rmx)
+            rmx=avc->n[i];
+    }
+    cmx++; //dealing with indices
+    rmx++;
+    printf("grid %i rows x %i cols\n", rmx, cmx); 
+    if(xy==0) {
+        for(r=0;r<v;++r) {
+            for(c=0;c<cmx;++c) {
+                if((c==avc->m[k]) && (r==avc->n[k])) {
+                    putchar('#');
+                    k++;
+                } else
+                    putchar('.');
+            }
+            putchar('\n');
+        }
     }
 
     return;
@@ -109,22 +229,6 @@ void app_avc(av_c *avc, int m, int n)
     CONDREALLOCMN(avc->vsz, avc->vbf, GBUF, avc->m, avc->n, int);
     avc->m[avc->vsz]=m;
     avc->n[avc->vsz++]=n;
-    return;
-}
-
-void memexact_avc(av_c *avc)
-{
-    /* somewhat trivial, but idea is that, as avc is a container, it can be re-alloced inside a function */
-    avc->m=realloc(avc->m, avc->vsz*sizeof(int));
-    avc->n=realloc(avc->n, avc->vsz*sizeof(int));
-    return;
-}
-
-void free_avc(av_c *avc)
-{
-    free(avc->m);
-    free(avc->n);
-    free(avc);
     return;
 }
 
@@ -185,6 +289,7 @@ int main(int argc, char *argv[])
     // int **ma=malloc(la->asz*sizeof(int*));
     char *tk=NULL, *tk2=NULL;
     char t[]=",";
+    char t2[]="=";
     av_c *avc=crea_avc(GBUF);
     for(i=0;i<la->asz;++i) {
         tk=strtok(la->l[i], t);
@@ -194,11 +299,46 @@ int main(int argc, char *argv[])
         tk2=strtok(NULL, t);
         app_avc(avc, atoi(tk), atoi(tk2));
     }
+    int rmx=0, cmx=0;
+    for(i=0;i<avc->vsz;++i) {
+        if(avc->m[i]>cmx)
+            cmx=avc->m[i];
+        if(avc->n[i]>rmx)
+            rmx=avc->n[i];
+    }
+    rmx++;
+    cmx++;
+    int l=i+1;
+    av_c *av2=crea_avc(GBUF);
+    int tks, xy;
+    for(i=l;i<la->asz;++i) {
+        // printf("%s\n", la->l[i]);
+        tk=strtok(la->l[i], t2);
+        if(tk==NULL) {
+            break;
+        }
+        tks=strlen(tk);
+        xy=(tk[tks-1]=='x')?0:1; // encode due to av_c limitations 0 for cols (x), 1 for rows (y)
+        tk2=strtok(NULL, t2);
+        app_avc(av2, xy, atoi(tk2));
+    }
     optis(avc);
+    prt_avc(av2);
     // prt_avc(avc);
-    findmx(avc);
+    // findmx(avc);
+    // findmx2(avc, 0, 7);
+    printf("proc %i %i %i %i\n", av2->m[0], av2->n[0], rmx, cmx);
+    proc_avc(avc, av2->m[0], av2->n[0], rmx, cmx);
+    prt_avc(avc);
+    findmx0(avc, av2->n[0], cmx);
+    // (av2->m[0]==1)?findmx0(avc, av2->n[0], cmx):findmx0(avc, av2->n[0], cmx); // correct call
+    printf("proc %i %i %i %i\n", av2->m[1], av2->n[1], rmx, cmx);
+    proc_avc(avc, av2->m[1], av2->n[1], rmx, cmx);
+    prt_avc(avc);
+    findmx0(avc, av2->n[0], av2->n[1]); // correct call
     memexact_avc(avc);
     free_avc(avc);
+    free_avc(av2);
     for(i=0;i<la->asz;++i)
         free(la->l[i]);
     free(la->l);
